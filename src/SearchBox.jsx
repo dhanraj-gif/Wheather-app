438dca3458e480fb99f35fcce2a79b92af9727aa
dhanraj gif <dhanrajkamuni7@gmail.com>
2025-09-14 11:36:47 +0530

    Updated gitignore, env, and SearchBox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}){
 	let [city,setCity]=useState("");
 	let [error,setError]=useState(false);
 	
-	const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+	const API_URL = process.env.REACT_APP_WEATHER_API_URL;
 	const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 	
